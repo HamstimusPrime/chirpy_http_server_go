115e7726171daf48842649ecd6332934848cc0ef
Mohammed Dadi <artofmohammed@gmail.com>
2025-04-11 04:29:40 +0100

    Add request metrics tracking and reporting
    - Create apiConfig struct with atomic counter for fileserver hits
    - Implement middleware to increment hit counter
    - Add /metrics endpoint to display hit count
    - Add /reset endpoint to reset counter to zero
    - Wrap fileserver handler with metrics middleware

--- main.go
+++ main.go
@@ -4,7 +4,38 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) middleWareWriteMetrics(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+
+	hitRequest := cfg.fileserverHits
+	numRequest := fmt.Sprintf("Hits: %d", hitRequest.Load())
+	w.Write([]byte(numRequest))
+}
+
+func (cfg *apiConfig) resetMetricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	cfg.fileserverHits = atomic.Int32{}
+}
 
 func main() {
 	port := "8080"
@@ -12,7 +43,13 @@ func main() {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("."))
-	mux.Handle("/", fileServer)
+
+	apiConfiguration := apiConfig{}
+	handler := http.StripPrefix("/app", fileServer)
+	mux.Handle("/app/", apiConfiguration.middlewareMetricsInc(handler))
+	mux.HandleFunc("/healthz", readinessHandler)
+	mux.HandleFunc("/metrics", apiConfiguration.metricsHandler)
+	mux.HandleFunc("/reset", apiConfiguration.resetMetricsHandler)
 
 	server := &http.Server{
 		Addr:    ":" + port,
@@ -21,4 +58,5 @@ func main() {
 	fmt.Printf("server running on port: %v\n", port)
 
 	log.Fatal(server.ListenAndServe())
+
 }
